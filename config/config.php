e7b99520d016028ef12ee5b999de50b6828a50a8
George John <george@renfos.com>
2018-10-05 13:22:23 +0530

    Translation bug fix

--- config/config.php
+++ config/config.php
@@ -34,7 +34,7 @@ return [
         'slugs'        => ['slug' => 'name'],
         'dates'        => ['deleted_at', 'updated_at', 'created_at'],
         'fillable'     => ['heading', 'meta_title', 'name', 'slug', 'order', 'view', 'compile', 'status', 'upload_folder', 'content', 'meta_keyword', 'abstract'],
-        'translatable'    => ['name', 'heading', 'content', 'meta_title', 'meta_keyword', 'meta_description'],
+        'translatable'    => ['title', 'heading', 'content', 'meta_title', 'meta_keyword', 'meta_description'],
         'upload_folder' => '/page/page',
         'uploads'      => [
             'banner' => [
